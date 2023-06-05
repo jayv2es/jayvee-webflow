81f1fe36f5bb9ab11531961f386efc984a618e6f
Jay Vee <joel98@hispeed.ch>
2023-06-05 19:56:23 +0200

    Added cookies.

--- custom-footer.js
+++ custom-footer.js
@@ -14,10 +14,13 @@ function hexToRgb(hex) {
 }
 
 function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + d.toUTCString() + ";";
+    if(exdays == 0) {
+        expires = "";
+    };
+    document.cookie = cname + "=" + cvalue + ";" + expires + "path=/;SameSite=None;Secure";
 }
 
 function getCookie(cname) {
@@ -38,7 +41,7 @@ function getCookie(cname) {
 
 // INIT FUNCTIONS
 
-function initializeButton(element, color) {
+function initializeArrowButton(element, color) {
     /*  
     ACTION:		Initializes an arrow button in a given color.
     -----
@@ -68,15 +71,15 @@ function initializeButton(element, color) {
 
 function initializeIconBox(element, color) {
     /*
-        ACTION:		Initializes an icon box in a given color.
-        -----
-        PARAMS:		element		HTML-element to be animated
-                    color		The color of the button (i.e. ring + arrow when not 									animated)
-                                -> Input as HEX-String, e.g. "#ABCDEF"
-        FLAGS:		-
-        -----
-        RETURNS:		-
-        */
+    ACTION:		Initializes an icon box in a given color.
+    -----
+    PARAMS:		element		HTML-element to be animated
+                color		The color of the button (i.e. ring + arrow when not animated)
+                            -> Input as HEX-String, e.g. "#ABCDEF"
+    FLAGS:		-
+    -----
+    RETURNS:		-
+    */
     setTimeout(() => {
         var svg = element.find('svg');
         var paths = svg.find('path');
@@ -92,7 +95,7 @@ function initializeIconBox(element, color) {
 
 // ANIMATION FUNCTIONS
 
-function arrowButton(element, reverse) {
+function hoverArrowButton(element, reverse) {
     /*
     ACTION:		Plays animation when hovered on button's div.
     -----
@@ -145,7 +148,7 @@ function hoverIconBox(element, reverse, color) {
     }, 0)
 }
 
-function clickIconBox(container, element, colorPre, colorPost, formField=false) {
+function clickIconBox(container, element, colorPre, colorPost, formField=false, cookieKey=false) {
     /*
     ACTION:		Changes design of button when clicked and executes action
     -----
@@ -154,6 +157,9 @@ function clickIconBox(container, element, colorPre, colorPost, formField=false) 
                 colorPre		The color of the button (i.e. ring + arrow when not animated)
                 colorPost		The color of the button after being clicked
                                     -> Input as HEX-String, e.g. "#ABCDEF"
+                *formField      If icon box part of a form, formField is the jQuery-object of the form field
+                                the answer has to be put in.
+                *cookieKey      The key (string) of the cookie where the answer should be stored.
     FLAGS:		-
     -----
     RETURNS:	-
@@ -163,7 +169,7 @@ function clickIconBox(container, element, colorPre, colorPost, formField=false) 
         var paths = svg.find('path');
         var text = element.find('h3')[0];
         for(var i=0; i<paths.length; i++) {
-        paths[i].style.fill = colorPost;
+            paths[i].style.fill = colorPost;
         };
         text.style.color = colorPost;
         element.css('background-color', `rgba(${hexToRgb(colorPre).r}, ${hexToRgb(colorPre).g}, ${hexToRgb(colorPre).b}, 1)`);
@@ -183,14 +189,19 @@ function clickIconBox(container, element, colorPre, colorPost, formField=false) 
             // Now initialize and reactivate event listeners
             initializeIconBox(currElement, colorPre);
             currElement.on("mouseenter", () => {
-            hoverIconBox(currElement, false, colorPre);
+                hoverIconBox(currElement, false, colorPre);
             });
             currElement.on("mouseleave", () => {
-            hoverIconBox(currElement, true, colorPre);
+                hoverIconBox(currElement, true, colorPre);
             });
             currElement.on("click", () => {
-            clickIconBox(container, currElement, colorPre, colorPost);
-            formField.val(currElement.find('h3')[0].innerHTML);
+                clickIconBox(container, currElement, colorPre, colorPost, formField, cookieKey);
+                if(formField) {
+                    formField.val(currElement.find('h3')[0].innerHTML);
+                };
+                if(cookieKey) {
+                    setCookie(cookieKey, currElement.find('h3')[0].innerHTML, 30);
+                };
             });
         };
         };
@@ -201,15 +212,15 @@ $(document).ready(() => {
     /* --------------------------------------------------------------------
     INITIALIZE
     -------------------------------------------------------------------- */
-        initializeButton($('#main-intro-button-1'), '#000000');
-        initializeButton($('#main-summary-button-1'), '#FFFFFF');
-        initializeButton($('#main-summary-button-2'), '#FFFFFF');
-        initializeButton($('#main-summary-button-3'), '#FFFFFF');
-        initializeButton($('#main-services-edu-button'), '#969939');
-        initializeButton($('#main-services-des-button'), '#992D29');
-        initializeButton($('#main-services-prod-button'), '#376D99');
-        initializeButton($('#edu-intro-button-1'), '#FFFFFF');
-        initializeButton($('#edu-intro-button-2'), '#FFFFFF');
+        initializeArrowButton($('#main-intro-button-1'), '#000000');
+        initializeArrowButton($('#main-summary-button-1'), '#FFFFFF');
+        initializeArrowButton($('#main-summary-button-2'), '#FFFFFF');
+        initializeArrowButton($('#main-summary-button-3'), '#FFFFFF');
+        initializeArrowButton($('#main-services-edu-button'), '#969939');
+        initializeArrowButton($('#main-services-des-button'), '#992D29');
+        initializeArrowButton($('#main-services-prod-button'), '#376D99');
+        initializeArrowButton($('#edu-intro-button-1'), '#FFFFFF');
+        initializeArrowButton($('#edu-intro-button-2'), '#FFFFFF');
 
         initializeIconBox($('#edu-tutoring-form-1-1'), '#FFFFFF');
         initializeIconBox($('#edu-tutoring-form-1-2'), '#FFFFFF');
@@ -225,59 +236,59 @@ $(document).ready(() => {
     ANIMATION: BUTTONS
     -------------------------------------------------------------------- */
         $('#main-intro-button-1').on("mouseenter", () => {
-            arrowButton($('#main-intro-button-1'), false);
+            hoverArrowButton($('#main-intro-button-1'), false);
         });
         $('#main-intro-button-1').on("mouseleave", () => {
-            arrowButton($('#main-intro-button-1'), true);
+            hoverArrowButton($('#main-intro-button-1'), true);
         });
 
         $('#main-summary-button-2').on("mouseenter", () => {
-            arrowButton($('#main-summary-button-2'), false);
+            hoverArrowButton($('#main-summary-button-2'), false);
         });
         $('#main-summary-button-2').on("mouseleave", () => {
-            arrowButton($('#main-summary-button-2'), true);
+            hoverArrowButton($('#main-summary-button-2'), true);
         });
 
         $('#main-summary-button-3').on("mouseenter", () => {
-            arrowButton($('#main-summary-button-3'), false);
+            hoverArrowButton($('#main-summary-button-3'), false);
         });
         $('#main-summary-button-3').on("mouseleave", () => {
-            arrowButton($('#main-summary-button-3'), true);
+            hoverArrowButton($('#main-summary-button-3'), true);
         });
 
         $('#main-services-edu-button').on("mouseenter", () => {
-            arrowButton($('#main-services-edu-button'), false);
+            hoverArrowButton($('#main-services-edu-button'), false);
         });
         $('#main-services-edu-button').on("mouseleave", () => {
-            arrowButton($('#main-services-edu-button'), true);
+            hoverArrowButton($('#main-services-edu-button'), true);
         });
 
         $('#main-services-des-button').on("mouseenter", () => {
-            arrowButton($('#main-services-des-button'), false);
+            hoverArrowButton($('#main-services-des-button'), false);
         });
         $('#main-services-des-button').on("mouseleave", () => {
-            arrowButton($('#main-services-des-button'), true);
+            hoverArrowButton($('#main-services-des-button'), true);
         });
 
         $('#main-services-prod-button').on("mouseenter", () => {
-            arrowButton($('#main-services-prod-button'), false);
+            hoverArrowButton($('#main-services-prod-button'), false);
         });
         $('#main-services-prod-button').on("mouseleave", () => {
-            arrowButton($('#main-services-prod-button'), true);
+            hoverArrowButton($('#main-services-prod-button'), true);
         });
 
         $('#edu-intro-button-1').on("mouseenter", () => {
-            arrowButton($('#edu-intro-button-1'), false);
+            hoverArrowButton($('#edu-intro-button-1'), false);
         });
         $('#edu-intro-button-1').on("mouseleave", () => {
-            arrowButton($('#edu-intro-button-1'), true);
+            hoverArrowButton($('#edu-intro-button-1'), true);
         });
 
         $('#edu-intro-button-2').on("mouseenter", () => {
-            arrowButton($('#edu-intro-button-2'), false);
+            hoverArrowButton($('#edu-intro-button-2'), false);
         });
         $('#edu-intro-button-2').on("mouseleave", () => {
-            arrowButton($('#edu-intro-button-2'), true);
+            hoverArrowButton($('#edu-intro-button-2'), true);
         });
 
     /* --------------------------------------------------------------------
@@ -290,8 +301,8 @@ $(document).ready(() => {
             hoverIconBox($('#edu-tutoring-form-1-1'), true, '#FFFFFF');
         });
         $('#edu-tutoring-form-1-1').on("click", () => {
-            clickIconBox($('#edu-tutoring-form-1'), $('#edu-tutoring-form-1-1'), $('#edu-tutoring-form-field-1'), '#FFFFFF', '#969939');
-            setCookie("edu-tutoring-input-1", $('#edu-tutoring-form-1-1').find('h3')[0].innerHTML, 30);
+            clickIconBox($('#edu-tutoring-form-1'), $('#edu-tutoring-form-1-1'), '#FFFFFF', '#969939', false, "edu-tutoring-input-1");
+            
         });
 
         $('#edu-tutoring-form-1-2').on("mouseenter", () => {
@@ -301,8 +312,7 @@ $(document).ready(() => {
             hoverIconBox($('#edu-tutoring-form-1-2'), true, '#FFFFFF');
         });
         $('#edu-tutoring-form-1-2').on("click", () => {
-            clickIconBox($('#edu-tutoring-form-1'), $('#edu-tutoring-form-1-2'), $('#edu-tutoring-form-field-1'), '#FFFFFF', '#969939');
-            setCookie("edu-tutoring-input-1", $('#edu-tutoring-form-1-2').find('h3')[0].innerHTML, 30);
+            clickIconBox($('#edu-tutoring-form-1'), $('#edu-tutoring-form-1-2'), '#FFFFFF', '#969939', false, "edu-tutoring-input-1");
         });
 
         $('#edu-tutoring-form-1-3').on("mouseenter", () => {
@@ -312,8 +322,7 @@ $(document).ready(() => {
             hoverIconBox($('#edu-tutoring-form-1-3'), true, '#FFFFFF');
         });
         $('#edu-tutoring-form-1-3').on("click", () => {
-            clickIconBox($('#edu-tutoring-form-1'), $('#edu-tutoring-form-1-3'), $('#edu-tutoring-form-field-1'), '#FFFFFF', '#969939');
-            setCookie("edu-tutoring-input-1", $('#edu-tutoring-form-1-3').find('h3')[0].innerHTML, 30);
+            clickIconBox($('#edu-tutoring-form-1'), $('#edu-tutoring-form-1-3'), '#FFFFFF', '#969939', false, "edu-tutoring-input-1");
         });
 
         $('#edu-tutoring-form-2-1').on("mouseenter", () => {
@@ -323,8 +332,7 @@ $(document).ready(() => {
             hoverIconBox($('#edu-tutoring-form-2-1'), true, '#FFFFFF');
         });
         $('#edu-tutoring-form-2-1').on("click", () => {
-            clickIconBox($('#edu-tutoring-form-2'), $('#edu-tutoring-form-2-1'), $('#edu-tutoring-form-field-2'), '#FFFFFF', '#969939');
-            setCookie("edu-tutoring-input-2", $('#edu-tutoring-form-2-1').find('h3')[0].innerHTML, 30);
+            clickIconBox($('#edu-tutoring-form-2'), $('#edu-tutoring-form-2-1'), '#FFFFFF', '#969939', false, "edu-tutoring-input-2");
         });
 
         $('#edu-tutoring-form-2-2').on("mouseenter", () => {
@@ -334,8 +342,7 @@ $(document).ready(() => {
             hoverIconBox($('#edu-tutoring-form-2-2'), true, '#FFFFFF');
         });
         $('#edu-tutoring-form-2-2').on("click", () => {
-            clickIconBox($('#edu-tutoring-form-2'), $('#edu-tutoring-form-2-2'), $('#edu-tutoring-form-field-2'), '#FFFFFF', '#969939');
-            setCookie("edu-tutoring-input-2", $('#edu-tutoring-form-2-2').find('h3')[0].innerHTML, 30);
+            clickIconBox($('#edu-tutoring-form-2'), $('#edu-tutoring-form-2-2'), '#FFFFFF', '#969939', false, "edu-tutoring-input-2");
         });
 
         $('#edu-tutoring-form-2-3').on("mouseenter", () => {
@@ -345,8 +352,7 @@ $(document).ready(() => {
             hoverIconBox($('#edu-tutoring-form-2-3'), true, '#FFFFFF');
         });
         $('#edu-tutoring-form-2-3').on("click", () => {
-            clickIconBox($('#edu-tutoring-form-2'), $('#edu-tutoring-form-2-3'), $('#edu-tutoring-form-field-2'), '#FFFFFF', '#969939');
-            setCookie("edu-tutoring-input-2", $('#edu-tutoring-form-2-3').find('h3')[0].innerHTML, 30);
+            clickIconBox($('#edu-tutoring-form-2'), $('#edu-tutoring-form-2-3'), '#FFFFFF', '#969939', false, "edu-tutoring-input-2");
         });
 
         $('#edu-tutoring-form-3-1').on("mouseenter", () => {
@@ -356,8 +362,7 @@ $(document).ready(() => {
             hoverIconBox($('#edu-tutoring-form-3-1'), true, '#FFFFFF');
         });
         $('#edu-tutoring-form-3-1').on("click", () => {
-            clickIconBox($('#edu-tutoring-form-3'), $('#edu-tutoring-form-3-1'), $('#edu-tutoring-form-field-3'), '#FFFFFF', '#969939');
-            setCookie("edu-tutoring-input-3", $('#edu-tutoring-form-3-1').find('h3')[0].innerHTML, 30);
+            clickIconBox($('#edu-tutoring-form-3'), $('#edu-tutoring-form-3-1'), '#FFFFFF', '#969939', false, "edu-tutoring-input-3");
         });
 
         $('#edu-tutoring-form-3-2').on("mouseenter", () => {
@@ -367,8 +372,7 @@ $(document).ready(() => {
             hoverIconBox($('#edu-tutoring-form-3-2'), true, '#FFFFFF');
         });
         $('#edu-tutoring-form-3-2').on("click", () => {
-            clickIconBox($('#edu-tutoring-form-3'), $('#edu-tutoring-form-3-2'), $('#edu-tutoring-form-field-3'), '#FFFFFF', '#969939');
-            setCookie("edu-tutoring-input-3", $('#edu-tutoring-form-3-2').find('h3')[0].innerHTML, 30);
+            clickIconBox($('#edu-tutoring-form-3'), $('#edu-tutoring-form-3-2'), '#FFFFFF', '#969939', false, "edu-tutoring-input-3");
         });
 
         $('#edu-tutoring-form-3-3').on("mouseenter", () => {
@@ -378,7 +382,7 @@ $(document).ready(() => {
             hoverIconBox($('#edu-tutoring-form-3-3'), true, '#FFFFFF');
         });
         $('#edu-tutoring-form-3-3').on("click", () => {
-            clickIconBox($('#edu-tutoring-form-3'), $('#edu-tutoring-form-3-3'), $('#edu-tutoring-form-field-3'), '#FFFFFF', '#969939');
-            setCookie("edu-tutoring-input-3", $('#edu-tutoring-form-3-3').find('h3')[0].innerHTML, 30);
+            clickIconBox($('#edu-tutoring-form-3'), $('#edu-tutoring-form-3-3'), '#FFFFFF', '#969939', false, "edu-tutoring-input-3");
+            console.log(getCookie("edu-tutoring-input-3"), getCookie("edu-tutoring-input-2"), getCookie("edu-tutoring-input-1"));
         });
 });
